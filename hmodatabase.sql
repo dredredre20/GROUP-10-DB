fb97ba7d38c00e18da6b3c796ec7fd19411e539e
Andre <andrechu17@gmail.com>
2024-11-08 22:59:31 +0800

    Update hmodatabase.sql

--- hmodatabase.sql
+++ hmodatabase.sql
@@ -1,38 +1,28 @@
 CREATE DATABASE IF NOT EXISTS `HMO`;
 USE `HMO`;
 
-DROP TABLE IF EXISTS `patients`;
-CREATE TABLE `patients` (
-  `patientID` int(10) NOT NULL,
-  `patientLastName` varchar(50) NOT NULL,
-  `patientFirstName` varchar(50) NOT NULL,
-  `sex` varchar(1) NOT NULL,
-  `birthday` datetime NOT NULL,
+DROP TABLE IF EXISTS `address`;
+CREATE TABLE `address`(
+	`addressID` int(10) NOT NULL,
+    `streetAddress` varchar(50) NOT NULL, 
+    `city` varchar(50) NOT NULL, 
+    `province` varchar(50) NOT NULL, 
+    `postalCode` varchar(20) NOT NULL,
+    PRIMARY KEY(`addressID`)
+);
+  
+DROP TABLE IF EXISTS `departments`;
+CREATE TABLE `departments` (
+  `departmentID` int(10) NOT NULL,
+  `departmentName` varchar(50) NOT NULL,
   `phoneNumber` varchar(50) NOT NULL,
-  `address` varchar(100) NOT NULL,
-  `contactPersonName` varchar(50) NOT NULL,
-  `contactPersonNumber` varchar(50) NOT NULL,
-  `bloodType` varchar(2) NOT NULL,
-  `allergies` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`patientID`)
+  `addressID` int(10) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `capacity` int(3) NOT NULL,
+  PRIMARY KEY (`departmentID`),
+  FOREIGN KEY (`addressID`) REFERENCES `address` (`addressID`)
   );
-
-DROP TABLE IF EXISTS `consultations`;
-CREATE TABLE `consultations` (
-  `consultationID` int(10) NOT NULL,
-  `patientID` int(10) NOT NULL,
-  `startDate` datetime NOT NULL,
-  `satisfactionRating` int(3) NOT NULL,
-  `endDate` datetime NOT NULL,
-  `reason` varchar(100) NOT NULL,
-  `doctorID` int(10) NOT NULL,
-  `notes` varchar(100) DEFAULT NULL,
-  `medicalHistory` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`consultationID`),
-  FOREIGN KEY (`patientID`) REFERENCES `patients` (`patientID`),
-  FOREIGN KEY(`doctorID`) REFERENCES `doctors` (`doctorID`)
-  );
-
+  
 DROP TABLE IF EXISTS `doctors`;
 CREATE TABLE `doctors` (
   `doctorID` int(10) NOT NULL,
@@ -43,27 +33,51 @@ CREATE TABLE `doctors` (
   `salary` int(10) NOT NULL, 
   `birthday` datetime NOT NULL,
   `phoneNumber` varchar(50) NOT NULL,
-  `address` varchar(100) NOT NULL,
+  `addressID` int(10) NOT NULL,
   `licenseNumber` varchar(50) NOT NULL,
   `field` varchar(50) NOT NULL,
   `specialization` varchar(50) NOT NULL,
   `workingHours` varchar(50) DEFAULT NULL,
-  
-  PRIMARY KEY (`doctorID`)
+  PRIMARY KEY (`doctorID`),
+  FOREIGN KEY (`departmentID`) REFERENCES `departments` (`departmentID`),
+  FOREIGN KEY (`addressID`) REFERENCES `address` (`addressID`)
   );
+
+ALTER TABLE `departments`
+ADD COLUMN `headDoctor` int(10),
+ADD FOREIGN KEY (`headDoctor`) REFERENCES `doctors` (`doctorID`);
   
-DROP TABLE IF EXISTS `departments`;
-CREATE TABLE `departments` (
-  `departmentID` int(10) NOT NULL,
-  `departmentName` varchar(50) NOT NULL,
+  
+DROP TABLE IF EXISTS `patients`;
+CREATE TABLE `patients` (
+  `patientID` int(10) NOT NULL,
+  `patientLastName` varchar(50) NOT NULL,
+  `patientFirstName` varchar(50) NOT NULL,
+  `sex` varchar(1) NOT NULL,
+  `birthday` datetime NOT NULL,
   `phoneNumber` varchar(50) NOT NULL,
-  `address` varchar(100) NOT NULL,
-  `headDoctor` int(10) DEFAULT NULL,
-  `email` varchar(50) NOT NULL,
-  `capacity` int(3) NOT NULL,
-  PRIMARY KEY (`departmentID`),
-  FOREIGN KEY (`headDoctor`) REFERENCES `doctors` (`doctorID`)
+  `addressID` int(10) NOT NULL,
+  `contactPersonName` varchar(50) NOT NULL,
+  `contactPersonNumber` varchar(50) NOT NULL,
+  `bloodType` varchar(2) NOT NULL,
+  `allergies` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`patientID`),
+  FOREIGN KEY (`addressID`) REFERENCES `address` (`addressID`)
   );
   
-ALTER TABLE `doctors`
-ADD FOREIGN KEY (`departmentID`) REFERENCES `departments` (`departmentID`);
+DROP TABLE IF EXISTS `consultations`;
+CREATE TABLE `consultations` (
+  `consultationID` int(10) NOT NULL,
+  `patientID` int(10) NOT NULL,
+  `startDate` datetime NOT NULL,
+  `satisfactionRating` int(3) NOT NULL,
+  `endDate` datetime NOT NULL,
+  `reason` varchar(100) NOT NULL,
+  `doctorID` int(10) NOT NULL,
+  `notes` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`consultationID`),
+  FOREIGN KEY (`patientID`) REFERENCES `patients` (`patientID`),
+  FOREIGN KEY(`doctorID`) REFERENCES `doctors` (`doctorID`)
+  );
+  
+
