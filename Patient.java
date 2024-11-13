ee3885f070b3c0811adc5d58a41aaf6a3036804c
gianprog3 <gian_martin_carlos@dlsu.edu.ph>
2024-11-13 11:37:03 +0800

    Update Patient.java

--- Patient.java
+++ Patient.java
@@ -7,12 +7,11 @@ class Patient {
     String emergencyContactNumber;
     String bloodType;
     String allergies;
-    int addressID;
-    Address pAddress;
+    String address;
 
     //initialize patient details
     public Patient(int patientID, String lastName, String firstName, String sex, String birthday, String contactNumber, String emergencyContactPerson, 
-        String emergencyContactNumber,String address, String bloodType, String allergies, int addressID, Address pAddress, String streetAddress, String city,
+        String emergencyContactNumber,String address, String bloodType, String allergies, String address, String streetAddress, String city,
         String province, String postalCode) {
         
         this.patientID = patientID;
@@ -25,28 +24,6 @@ class Patient {
         this.emergencyContactNumber = emergencyContactNumber;
         this.bloodType = bloodType;
         this.allergies = allergies;
-        this.addressID = addressID;
-
-        //create an address for the patient
-        this.pAddress = new Address(addressID, streetAddress, city, province, postalCode);
-
+        this.address = streetAddress + city + province + postalCode;
     }
-
-    //when address is updated
-    public void updateAddress(String streetAddress, String city, String province, String postalCode) {
-        this.pAddress = new Address(this.addressID, streetAddress, city, province, postalCode);
-    }
-
-    //return the address
-    public Address getAddress(){
-        return pAddress;
-    }
-
-    //return the addressID
-    public int getAddressID(){
-        return addressID;
-    }
-
-
-
 }
