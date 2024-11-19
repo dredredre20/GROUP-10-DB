e9e73422eef5ec8fb52c4834c1ab763fc6ff240f
Andre <andrechu17@gmail.com>
2024-11-19 14:56:04 +0800

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,8 +1,21 @@
 public class Driver {
-
     public static void main(String[] args) {
-	View view = new View();
-	Model model = new Model();
-        Controller controller = new Controller(view, model);
+        try {
+            // Load MySQL JDBC Driver explicitly
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            
+            // First populate the data
+            PopulateData.main(null);
+            
+            // Then initialize MVC components
+            View view = new View();
+            Model model = new Model();
+            Controller controller = new Controller(view, model);
+            
+        } catch (ClassNotFoundException e) {
+            System.err.println("MySQL JDBC Driver not found.");
+            System.err.println("Make sure mysql-connector-j-9.1.0.jar is in your classpath");
+            e.printStackTrace();
+        }
     }
 }
