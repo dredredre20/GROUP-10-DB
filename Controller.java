f40f7b12a50b1eee7f7f8d7661b85a75d6e73063
gianprog3 <gian_martin_carlos@dlsu.edu.ph>
2024-11-19 15:46:09 +0800

    Updated for final proposal

--- Controller.java
+++ Controller.java
@@ -18,37 +18,76 @@ public class Controller{
 	    public void actionPerformed(ActionEvent e) {
 		view.refresh();
 		switch(view.getMenu()) {
-		    case 100: 
-		    case 200: 
-		    case 300: 
-		    case 400: 
-		    case 500:
-		    case 600:
-		    case 700:
+		    case 1000: 
+		    case 2000: 
+		    case 3000: 
+		    case 4000: 
+		    case 5000:
+		    case 6000:
+		    case 7000:
 		    	view.mainMenu();
 		    	break;
-		    case 110:
-		    case 120:
-			view.appoint();
+		    case 1100:
+		    case 1200:
+		    case 1300:
+			view.patient();
 			break;
-		    case 101:
-			view.mainMenu();
+		    case 1010:
+		    case 1020:
+			view.patientMenu();
 			break;
-		    case 210:
-			view.update();
+		    case 1011:
+		    case 1012:
+		    case 1013:
+			view.patientMenuEdit();
 			break;
-		    case 310:
+		    case 2100:
+			view.pUpdate();
+			break;
+		    case 3100:
 			view.consult();
 			break;
-		    case 410:
-		    case 420:
-		    case 430:
+		    case 3110:
+		    case 3120:
+		    case 3130:
+		    case 3140:
+			view.consultUpdate();
+			break;
+		    case 4100:
+		    case 4200:
+		    case 4300:
+		    case 4400:
+		    case 4500:
+		    case 4600:
 			view.remove();
 			break;
-		    case 510:
-			view.assign();
-		    case 610:
-			view.config();
+		    case 5100:
+		    case 5200:
+		    case 5300:
+			view.doctor();
+			break;
+		    case 5010:
+		    case 5020:
+		    case 5030:
+			view.doctorMenu();
+			break;
+		    case 6100:
+		    case 6200:
+		    case 6300:
+		    case 6400:
+		    case 6500:
+		    case 6600:
+			view.refer();
+			break;
+		    case 6210:
+			view.referLabEdit();
+			break;
+		    case 6410:
+			view.referCompEdit();
+			break;
+		    case 6610:
+			view.referMedEdit();
+			break;
 		}
 		view.setVisible();
 	    }
@@ -58,21 +97,82 @@ public class Controller{
 	    @Override
 	    public void actionPerformed(ActionEvent e) {
 		switch(view.getMenu()) {
-		    case 110: 
-		    case 120:
-		    case 101:
-		    case 200:
-		    case 210:
-		    case 300:
-		    case 310:
-		    case 410:
-		    case 420:
-		    case 430:
-		    case 500:
-		    case 510:
-		    case 600:
-		    case 610:
+		    case 1100: 
+		    case 1011:
+		    case 1012:
+		    case 1013:
+		    case 1020:
+		    case 2100:
+		    case 3110:
+		    case 3120:
+		    case 3130:
+		    case 3140:
+		    case 4100:
+		    case 4200:
+		    case 4300:
+		    case 4400:
+		    case 4500:
+		    case 4600:
+		    case 5100:
+		    case 5010:
+		    case 5020:
+		    case 5030:
+		    case 6100:
+		    case 6210:
+		    case 6300:
+		    case 6410:
+		    case 6500:
+		    case 6610:
 			view.setResult1("test");
+		    	break;
+		    case 1200:
+			view.refresh();
+			view.setText1("");
+			view.setResult1("");
+		    	view.patientMenu();
+			view.setVisible();
+		    	break;
+		    case 2000:
+			view.refresh();
+			view.setText1("");
+			view.setResult1("");
+		    	view.pUpdateData();
+			view.setVisible();
+		    	break;
+		    case 3000:
+			view.refresh();
+			view.setText1("");
+			view.setResult1("");
+		    	view.consultUpdate();
+			view.setVisible();
+		    	break;
+		    case 5200:
+			view.refresh();
+			view.setText1("");
+			view.setResult1("");
+		    	view.doctorMenu();
+			view.setVisible();
+		    	break;
+		    case 6200:
+			view.refresh();
+			view.setText1("");
+			view.setResult1("");
+		    	view.referLabEditData();
+			view.setVisible();
+		    	break;
+		    case 6400:
+			view.refresh();
+			view.setText1("");
+			view.setResult1("");
+		    	view.referCompEditData();
+			view.setVisible();
+		    	break;
+		    case 6600:
+			view.refresh();
+			view.setText1("");
+			view.setResult1("");
+		    	view.referMedEditData();
+			view.setVisible();
 		    	break;
 		}
 	    }
@@ -83,29 +183,75 @@ public class Controller{
 	    public void actionPerformed(ActionEvent e) {
 
 		switch(view.getMenu()) {
-		    case 000: 
+		    case 0000: 
 			view.refresh();
 			view.setText1("");
 			view.setResult1("");
-		    	view.appoint();
+		    	view.patient();
 			view.setVisible();
 		    	break;
 
-		    case 100:
+		    case 1000:
 			view.refresh();
 			view.setResult1("");
-		    	view.appointNew();
+		    	view.patientNew();
 			view.setVisible();
 			break;
 
-		    case 400:
+		    case 1300:
+			view.refresh();
+			view.setResult1("");
+		    	view.patientMenuEdit();
+			view.setVisible();
+			break;
+
+		    case 1010:
+			view.refresh();
+			view.setResult1("");
+		    	view.patientMenuEditAddC();
+			view.setVisible();
+			break;
+
+		    case 1020:
+			view.setResult1("test");
+			break;
+
+		    case 3100:
+			view.refresh();
+			view.setResult1("");
+		    	view.consultUpdateRating();
+			view.setVisible();
+			break;
+
+		    case 4000:
 			view.refresh();
 			view.setResult1("");
 		    	view.removePatient();
 			view.setVisible();
 			break;
 
-		    case 700:
+		    case 5000:
+			view.refresh();
+			view.setResult1("");
+		    	view.doctorNew();
+			view.setVisible();
+			break;
+
+		    case 5300:
+			view.refresh();
+			view.setResult1("");
+		    	view.doctorMenuAdd();
+			view.setVisible();
+			break;
+
+		    case 6000:
+			view.refresh();
+			view.setResult1("");
+		    	view.referLabAdd();
+			view.setVisible();
+			break;
+
+		    case 7000:
 			view.setResult1("test");
 			break;
 		}
@@ -118,29 +264,71 @@ public class Controller{
 	    public void actionPerformed(ActionEvent e) {
 
 		switch(view.getMenu()) {
-		    case 000: 
+		    case 0000: 
 			view.refresh();
 			view.setText1("");
 			view.setResult1("");
-		    	view.update();
+		    	view.pUpdate();
 			view.setVisible();
 		    	break;
 
-		    case 100:
+		    case 1000:
 			view.refresh();
 			view.setResult1("");
-		    	view.appointOld();
+		    	view.patientOld();
 			view.setVisible();
 			break;
 
-		    case 400:
+		    case 1300:
+			view.refresh();
+			view.setResult1("");
+		    	view.patientMenuConsult();
+			view.setVisible();
+			break;
+
+		    case 1010:
+			view.refresh();
+			view.setResult1("");
+		    	view.patientMenuEditRemoveC();
+			view.setVisible();
+			break;
+
+		    case 3100:
+			view.refresh();
+			view.setResult1("");
+		    	view.consultUpdateLab();
+			view.setVisible();
+			break;
+
+		    case 4000:
 			view.refresh();
 			view.setResult1("");
 		    	view.removeConsult();
 			view.setVisible();
 			break;
 
-		    case 700:
+		    case 5000:
+			view.refresh();
+			view.setResult1("");
+		    	view.doctorOld();
+			view.setVisible();
+			break;
+
+		    case 5300:
+			view.refresh();
+			view.setResult1("");
+		    	view.doctorMenuRemove();
+			view.setVisible();
+			break;
+
+		    case 6000:
+			view.refresh();
+			view.setResult1("");
+		    	view.referLabEdit();
+			view.setVisible();
+			break;
+
+		    case 7000:
 			view.setResult1("test");
 			break;
 		}
@@ -153,22 +341,50 @@ public class Controller{
 	    public void actionPerformed(ActionEvent e) {
 
 		switch(view.getMenu()) {
-		    case 000: 
+		    case 0000: 
 			view.refresh();
 			view.setText1("");
 			view.setResult1("");
-		    	view.remove();
+		    	view.consult();
 			view.setVisible();
 		    	break;
 
-		    case 400:
+		    case 1010:
+			view.refresh();
+			view.setResult1("");
+		    	view.patientMenuEditPatient();
+			view.setVisible();
+			break;
+
+		    case 3100:
+			view.refresh();
+			view.setResult1("");
+		    	view.consultUpdateDia();
+			view.setVisible();
+			break;
+
+		    case 4000:
 			view.refresh();
 			view.setResult1("");
 		    	view.removeDoctor();
 			view.setVisible();
 			break;
 
-		    case 700:
+		    case 5300:
+			view.refresh();
+			view.setResult1("");
+		    	view.doctorMenuEdit();
+			view.setVisible();
+			break;
+
+		    case 6000:
+			view.refresh();
+			view.setResult1("");
+		    	view.referComp();
+			view.setVisible();
+			break;
+
+		    case 7000:
 			view.setResult1("test");
 			break;
 		}
@@ -179,52 +395,118 @@ public class Controller{
 	this.getView().setButton4(new ActionListener() {
 	    @Override
 	    public void actionPerformed(ActionEvent e) {
-		view.refresh();
 
 		switch(view.getMenu()) {
-		    case 000: 
+		    case 0000: 
 			view.refresh();
 			view.setText1("");
 			view.setResult1("");
-		    	view.assign();
+		    	view.remove();
 			view.setVisible();
 		    	break;
 
-		    case 700:
+		    case 3100:
+			view.refresh();
+			view.setResult1("");
+		    	view.consultUpdatePre();
+			view.setVisible();
+			break;
+
+		    case 4000:
+			view.refresh();
+			view.setResult1("");
+		    	view.removeLab();
+			view.setVisible();
+			break;
+
+		    case 6000:
+			view.refresh();
+			view.setResult1("");
+		    	view.referCompEdit();
+			view.setVisible();
+			break;
+
+		    case 7000:
 			view.setResult1("test");
 			break;
 		}
-
-		view.setVisible();
 	    }
 	});
 
 	this.getView().setButton5(new ActionListener() {
 	    @Override
 	    public void actionPerformed(ActionEvent e) {
-		view.refresh();
 
 		switch(view.getMenu()) {
-		    case 000: 
+		    case 0000: 
 			view.refresh();
 			view.setText1("");
 			view.setResult1("");
-		    	view.config();
+		    	view.doctor();
 			view.setVisible();
 		    	break;
-		}
 
-		view.setVisible();
+		    case 4000:
+			view.refresh();
+			view.setResult1("");
+		    	view.removeComplaint();
+			view.setVisible();
+			break;
+
+		    case 6000:
+			view.refresh();
+			view.setResult1("");
+		    	view.referMed();
+			view.setVisible();
+			break;
+
+		    case 7000:
+			view.setResult1("test");
+			break;
+		}
 	    }
 	});
 
 	this.getView().setButton6(new ActionListener() {
 	    @Override
 	    public void actionPerformed(ActionEvent e) {
-		view.refresh();
 
 		switch(view.getMenu()) {
-		    case 000: 
+		    case 0000: 
+			view.refresh();
+			view.setText1("");
+			view.setResult1("");
+		    	view.refer();
+			view.setVisible();
+		    	break;
+
+		    case 4000:
+			view.refresh();
+			view.setResult1("");
+		    	view.removeMedicine();
+			view.setVisible();
+			break;
+
+		    case 6000:
+			view.refresh();
+			view.setResult1("");
+		    	view.referMedEdit();
+			view.setVisible();
+			break;
+
+		    case 7000:
+			view.setResult1("test");
+			break;
+		}
+	    }
+	});
+
+	this.getView().setButton7(new ActionListener() {
+	    @Override
+	    public void actionPerformed(ActionEvent e) {
+
+		switch(view.getMenu()) {
+		    case 0000: 
 			view.refresh();
 			view.setText1("");
 			view.setResult1("");
@@ -232,8 +514,6 @@ public class Controller{
 			view.setVisible();
 		    	break;
 		}
-
-		view.setVisible();
 	    }
 	});
     }
