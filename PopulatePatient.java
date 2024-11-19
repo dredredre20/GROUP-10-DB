b04d8dac6647124ea166b2f0d7b7212edf5227d7
Andre <andrechu17@gmail.com>
2024-11-19 14:58:19 +0800

    Update PopulatePatient.java

--- PopulatePatient.java
+++ PopulatePatient.java
@@ -1,7 +1,8 @@
 import java.sql.*;
 
 public class PopulatePatient {
-    public static void main(String[] args){
+
+    public void Patient(){
 
         String hmo_url = "jdbc:mysql://127.0.0.1:3306/hmo";
         String user = "root";
