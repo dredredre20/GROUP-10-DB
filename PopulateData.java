f533f0e96f2ccf324515033878956cfca8a66363
gianprog3 <gian_martin_carlos@dlsu.edu.ph>
2024-11-23 20:22:28 +0800

    FINAL

--- PopulateData.java
+++ PopulateData.java
@@ -1,15 +1,20 @@
 public class PopulateData {
     public static void main(String[] args) {
-        Transactions transactions = new Transactions("jdbc:mysql://127.0.0.1:3306/hmo", "root", "password");
-        PopulateDoctor doctor = new PopulateDoctor();
-        PopulatePatient patient = new PopulatePatient();
-        PopulateConsul1 consultations1 = new PopulateConsul1();
-        PopulateConsul2 consultations2 = new PopulateConsul2();
 
-        // Call the populate methods IF not called yet, otherwise leave commented out
-        //doctor.doctor();
-        //patient.Patient();
-        //consultations1.consultations1();
-        //consultations2.consultations2();
+	//ENTER DATABASE HERE 
+
+	String hmo = "jdbc:mysql://127.0.0.1:3306/hmo";
+	String user = "root";
+	String pass = "password";
+        Transactions transactions = new Transactions(hmo, user, pass);
+        PopulateDoctor doctor = new PopulateDoctor("hmo, user, pass);
+        PopulatePatient patient = new PopulatePatient(hmo, user, pass);
+        PopulateConsul1 consultations1 = new PopulateConsul1(hmo, user, pass);
+        PopulateConsul2 consultations2 = new PopulateConsul2(hmo, user, pass);
+
+        doctor.doctor();
+        patient.Patient();
+        consultations1.consultations1();
+        consultations2.consultations2();
     }
 }
