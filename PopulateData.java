e48f2ecf4ffa45c98b57d1e3418e97fb5d646a0d
gianprog3 <gian_martin_carlos@dlsu.edu.ph>
2024-11-23 20:22:54 +0800

    FINAL

--- PopulateData.java
+++ PopulateData.java
@@ -7,7 +7,7 @@ public class PopulateData {
 	String user = "root";
 	String pass = "password";
         Transactions transactions = new Transactions(hmo, user, pass);
-        PopulateDoctor doctor = new PopulateDoctor("hmo, user, pass);
+        PopulateDoctor doctor = new PopulateDoctor(hmo, user, pass);
         PopulatePatient patient = new PopulatePatient(hmo, user, pass);
         PopulateConsul1 consultations1 = new PopulateConsul1(hmo, user, pass);
         PopulateConsul2 consultations2 = new PopulateConsul2(hmo, user, pass);
